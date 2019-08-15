2eb5141d51f875b0a20ae6e5a05ca88922ae6082
Colt Tyler Wilson <colt.t.wilson@gmail.com>
2019-08-15 01:24:25 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import {increment} from './actions';
+import {decrement} from './actions';
+import {sign} from './actions';
 
 function App() {
+  const counter = useSelector(state => state.counterReducer);
+  const log = useSelector(state => state.loggedReducer);
+  const dispatch = useDispatch();
+
+  const buttonStyle = {
+    backgroundColor: '#4CAF50', /* Green */
+    border: 'none',
+    color: 'white',
+    padding: '15px 32px',
+    textAlign: 'center',
+    textDecoration: 'none',
+    display: 'inline-block',
+    fontSize: '16px',
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter: {counter}</h1>
+      <button style={buttonStyle} onClick={() => dispatch(increment(5))}>Increment</button>
+      <button style={buttonStyle} onClick={() => dispatch(decrement(2))}>Decrement</button>
+      <button style={buttonStyle} onClick={() => dispatch(sign())}>Log</button>
+      {log ? <h1>The Plan is the Plan</h1> : <h1>You get Nothing</h1>}
     </div>
   );
 }
